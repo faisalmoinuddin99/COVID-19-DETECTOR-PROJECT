eb9f5f3c8d212681158895980f22b9ffcfd146ca
faisalmoinuddin99 <51179625+faisalmoinuddin99@users.noreply.github.com>
2020-03-28 04:38:08 +0530

    Add files via upload

---
+++ COVID-19 DETECTOR PROJECT/main.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,request
+import pickle
+app = Flask(__name__)
+
+# open a file, where you stored the pickled data
+file = open('model.pkl', 'rb')
+
+clf = pickle.load(file)
+file.close()
+
+@app.route('/',methods = ["GET","POST"])
+def hello_world():
+    if request.method == 'POST':
+        myDict = request.form
+        fever = int(myDict['fever'])
+        age = int(myDict['age'])
+        pain = int(myDict['pain'])
+        runnyNose = int(myDict['runnyNose'])
+        diffBreath = int(myDict['diffBreath'])
+        # Code for inference
+        inputFeatures = [fever, pain, age, runnyNose, diffBreath]
+        infProb = clf.predict_proba([inputFeatures])[0][1]
+        print(infProb)
+        return render_template("show.html",inf = round(infProb*100))
+
+    return render_template("index.html")
+    #return 'Hello, World! ' + str(infProb)
+
+app.run(debug=True)
